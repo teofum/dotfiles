949b05f699751c1bd7c6e6188e893b5daa4fb3a4
Teo Fumagalli <teo.fum@gmail.com>
2025-03-15 18:37:57 -0300

    fish config for macos

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,16 @@
-nvm use --lts # Make sure npm is working
+nvm use --lts > /dev/null # Make sure npm is working
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+switch (uname)
+    case Linux
+        if status is-interactive
+            # Commands to run in interactive sessions can go here
+        end
 
-if status is-login
-    if test -z $DISPLAY; and test $XDG_VTNR = 1
-        exec startx
-    end
+        if status is-login
+            if test -z $DISPLAY; and test $XDG_VTNR = 1
+                exec startx
+            end
+        end
+    case Darwin
+        /opt/homebrew/bin/brew shellenv | source
 end
